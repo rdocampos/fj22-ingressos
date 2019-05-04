7f5ae13be0970f3fefa11476c3b844b8c12969e4
lab7998 <lab7998@caelumBSB01-04>
2019-05-04 14:37:21 -0300

    adicionado desconto no ingresso

---
+++ src/test/java/ingresso/DescontoTest.java
@@ -0,0 +1,34 @@
+package ingresso;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalTime;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Ingresso;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.descontos.SemDesconto;
+
+public class DescontoTest {
+
+	@Test
+	public void nao_deve_conceder_desconto_para_ingresso_normal() {
+
+		Sala sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
+		Filme filme = new Filme("Rogue	One", Duration.ofMinutes(120), "SCI-FI",
+				new BigDecimal("12.0"));
+
+		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+
+		Ingresso Ingresso = new Ingresso(sessao, new SemDesconto());
+
+		BigDecimal precoEsperado = new BigDecimal("32.50");
+
+		Assert.assertEquals(precoEsperado, Ingresso.getPreco());
+	}
+
+}
